5981ce7b6336cbd33a81f6e4a1b36923a2cec40f
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-23 09:54:05 -0300

    Project created

---
+++ src/main/java/com/projetowebjava/projetowebjava/ProjetowebjavaApplication.java
@@ -0,0 +1,13 @@
+package com.projetowebjava.projetowebjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowebjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowebjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetowebjava/projetowebjava/ProjetowebjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetowebjava.projetowebjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
