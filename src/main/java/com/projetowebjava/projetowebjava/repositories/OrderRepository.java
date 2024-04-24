0390765943e3b7af6c562ca66a0206ac38acf110
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-24 09:57:31 -0300

    Order, Instant, ISO 8601 Part 2

---
+++ src/main/java/com/projetowebjava/projetowebjava/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.projetowebjava.projetowebjava.repositories;
+
+import com.projetowebjava.projetowebjava.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
