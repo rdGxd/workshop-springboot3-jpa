0952e2395710e11c30b710aba5a5f3c4a4743531
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-25 10:07:43 -0300

    Payment, one-to-one association

--- src/main/java/com/projetowebjava/projetowebjava/entities/Order.java
+++ src/main/java/com/projetowebjava/projetowebjava/entities/Order.java
@@ -23,6 +23,9 @@ public class Order implements Serializable {
 
     private Integer orderStatus;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -76,6 +79,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
