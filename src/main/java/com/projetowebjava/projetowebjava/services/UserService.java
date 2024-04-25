9bb398f018638973e9f9e4987f8453ca9059c154
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-25 13:03:07 -0300

    Exception handling - update

--- src/main/java/com/projetowebjava/projetowebjava/services/UserService.java
+++ src/main/java/com/projetowebjava/projetowebjava/services/UserService.java
@@ -4,6 +4,7 @@ import com.projetowebjava.projetowebjava.entities.User;
 import com.projetowebjava.projetowebjava.repositories.UserRepository;
 import com.projetowebjava.projetowebjava.services.exceptions.DatabaseException;
 import com.projetowebjava.projetowebjava.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     public void updateData(User entity, User obj) {
